627329c095a5f0a3194f1d7cb9ffa1da730a4b31
puppe1990 <matheus.puppe90@hotmail.com>
2023-03-20 09:22:31 -0300

    first commit

---
+++ spec/bling_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BlingApi do
+  it "has a version number" do
+    expect(BlingApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
